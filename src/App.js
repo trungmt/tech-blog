b8eb22acb1b353b4b60a8e050dc67d0d9fa83616
trungmt <trungtm@bit-vietnam.com>
2020-09-24 01:08:52 +0700

    modify header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to TJ's blog</h1>
+          <h1 className="App-title">
+            Welcome to TJ's blog
+            <br /> Here is where I keep logging my tech life
+          </h1>
         </header>
         <h1>[2020-09-24] How to deploy React app into Netlify</h1>
         <ol>
-          <li><a href="https://www.youtube.com/watch?v=JwWvD_fWJFY">Register free account and link account with github</a></li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=JwWvD_fWJFY">
+              Register free account and link account with github
+            </a>
+          </li>
           <li>Deploy React project to Netlify</li>
           <li>Link Netlify host with own domain</li>
         </ol>
